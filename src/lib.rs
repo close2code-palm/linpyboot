512d7beeb6c27de4b268950209d2bd90938ed3b0
Juxan <julyfortune101@gmail.com>
2024-06-08 00:38:37 +0300

    fix of errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,48 @@
+use nix::errno::Errno;
 use nix::libc::{RB_DISABLE_CAD, RB_ENABLE_CAD};
 use nix::sys;
 use nix::sys::reboot::RebootMode;
-use pyo3::{Bound, pyfunction, pymodule, PyResult, wrap_pyfunction};
 use pyo3::prelude::PyModule;
+use pyo3::{create_exception, pyfunction, pymodule, wrap_pyfunction, Bound, PyResult};
+
+create_exception!(rebo, PermissionError, pyo3::exceptions::PyException);
+create_exception!(rebo, CommandError, pyo3::exceptions::PyException);
 
 #[pyfunction]
 fn reboot(mode: i64) -> PyResult<()> {
     if mode as i32 == RB_DISABLE_CAD {
-        sys::reboot::set_cad_enabled(false)?;
+        return handle_reboot_execution(sys::reboot::set_cad_enabled(false));
     } else if mode as u32 as i32 == RB_ENABLE_CAD {
-        sys::reboot::set_cad_enabled(true)?;
+        return handle_reboot_execution(sys::reboot::set_cad_enabled(true));
     } else {
         let reboot_mode = match mode as u32 {
             0xcdef0123u32 => RebootMode::RB_HALT_SYSTEM,
             0x45584543u32 => RebootMode::RB_KEXEC,
             0x4321fedcu32 => RebootMode::RB_POWER_OFF,
             0xd000fce2u32 => RebootMode::RB_SW_SUSPEND,
-            _ => RebootMode::RB_AUTOBOOT,
+            0x01234567u32 => RebootMode::RB_AUTOBOOT,
+            _ => {
+                return Err(CommandError::new_err(
+                    "Argument is unknown or not supported.",
+                ))
+            }
         };
         sys::reboot::reboot(reboot_mode).unwrap();
     }
     Ok(())
 }
 
+fn handle_reboot_execution<T>(execution: Result<(), T>) -> PyResult<()> {
+    return match execution {
+        Ok(_) => Ok(()),
+        Err(err) => match err {
+            Errno::EPERM => Err(PermissionError::new_err(
+                "Process needs CAP_SYS_BOOT capability set.",
+            )),
+            _ => Err(CommandError::new_err("Unknown argument.")),
+        },
+    };
+}
 
 #[pymodule]
 fn rebo(m: &Bound<'_, PyModule>) -> PyResult<()> {
