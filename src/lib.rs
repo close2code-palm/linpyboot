5f4f21da07c2ccb7e7ca26d77ba0aca049a59fa5
Juxan <julyfortune101@gmail.com>
2024-06-07 16:55:48 +0300

    out of bound size fix

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,12 @@ use pyo3::{Bound, pyfunction, pymodule, PyResult, wrap_pyfunction};
 use pyo3::prelude::PyModule;
 
 #[pyfunction]
-fn reboot(mode: i32) -> PyResult<()> {
-    let reboot_mode = match mode {
-        0xcdef0123 => RebootMode::RB_HALT_SYSTEM,
-        0x45584543 => RebootMode::RB_KEXEC,
-        0x4321fedc => RebootMode::RB_POWER_OFF,
-        0xd000fce2 => RebootMode::RB_SW_SUSPEND,
+fn reboot(mode: i64) -> PyResult<()> {
+    let reboot_mode = match mode as u32 {
+        0xcdef0123u32 => RebootMode::RB_HALT_SYSTEM,
+        0x45584543u32 => RebootMode::RB_KEXEC,
+        0x4321fedcu32 => RebootMode::RB_POWER_OFF,
+        0xd000fce2u32 => RebootMode::RB_SW_SUSPEND,
         _ => RebootMode::RB_AUTOBOOT,
     };
     sys::reboot::reboot(reboot_mode).unwrap();
